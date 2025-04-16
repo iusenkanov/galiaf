2b320d232702f1ee3e0046e20a07231991650b80
Iskender Usenkanov <iusenkanov@gmail.com>
2025-04-15 21:03:54 -0700

    Add deploy in docker

--- src/main/java/com/alpha/galiaf/GaliafApplication.java
+++ src/main/java/com/alpha/galiaf/GaliafApplication.java
@@ -9,12 +9,6 @@ public class GaliafApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GaliafApplication.class, args);
-		Sentry.init(options -> {
-			options.setDsn("https://ea8c8ebda74aa7be8d20b3131e43cccb@o4509091708928000.ingest.us.sentry.io/4509097435267072");
-			options.setDebug(true);
-			options.setSendDefaultPii(true);
-			options.setRelease("my-app@1.2.3");
-		});
 		Sentry.captureMessage("✅ Test log from Spring Boot main method (integration check)");
 	}
 
