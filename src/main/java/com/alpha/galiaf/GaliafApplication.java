3beaf823bd096a6e687f09e2126b80eb56eca376
Iskender Usenkanov <iusenkanov@gmail.com>
2025-04-18 15:41:48 -0700

    newone_30

--- src/main/java/com/alpha/galiaf/GaliafApplication.java
+++ src/main/java/com/alpha/galiaf/GaliafApplication.java
@@ -9,9 +9,6 @@ public class GaliafApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GaliafApplication.class, args);
-		Sentry.init(options -> {
-			options.addBundleId(System.getenv("DEBUG_ID"));
-		});
 		Sentry.captureMessage("✅ Test log from Spring Boot main method (integration check)");
 	}
 
